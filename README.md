e75c6d1e55d787b320e5b06f87e019489754d5fa
Rajeev B <rajeevrvis@gmail.com>
2023-02-26 19:26:17 +0530

    add alacritty and tmux config

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 
 Run ./setup.sh
+
--- setup.sh
+++ setup.sh
@@ -3,3 +3,6 @@
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
